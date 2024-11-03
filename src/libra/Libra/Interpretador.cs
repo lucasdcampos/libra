2c7446c6e3dcc99cc4b63c09356cf135dcbff83f
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2024-11-03 18:57:09 -0300

    Refatorado Interpretador

--- src/libra/Libra/Interpretador.cs
+++ src/libra/Libra/Interpretador.cs
@@ -15,153 +15,149 @@ public class Interpretador
 
     public void Interpretar(Programa programa)
     {
-        _programa = programa;
+        LibraBase.ProgramaAtual = _programa = programa;
         
-        LibraBase.ProgramaAtual = _programa;
-
         for(int i = 0; i < _programa.Instrucoes.Count; i++)
-        {
             InterpretarInstrucao(_programa.Instrucoes[i]);
-        }
-
-        LibraBase.ProgramaAtual = null;
+        
+        LibraBase.ProgramaAtual = null; // limpar o programa depois que terminar
     }
 
     private Instrucao InterpretarInstrucao(Instrucao instrucao)
     {
-        if (instrucao is InstrucaoSair)
-        {
-            var sair = (InstrucaoSair)instrucao;
-            int? codigoSaida = (int?)InterpretarExpressao(sair.Expressao);
-
-            if (codigoSaida == null)
-                new ErroAcessoNulo().LancarErro();
-
-            LibraBase.Sair((int)codigoSaida);
-        }
-        else if (instrucao is InstrucaoVar)
+        if (instrucao is InstrucaoVar)
         {
             var var = (InstrucaoVar)instrucao;
             DefinirVariavel(var.Identificador, var.EhDeclaracao, false, var.Valor, var.Tipo, var.IndiceVetor);
         }
-        else if (instrucao is InstrucaoConst)
-        {
-            var constante = (InstrucaoConst)instrucao;
-            
-            DefinirVariavel(constante.Identificador, true, true, constante.Expressao, TokenTipo.TokenInvalido, null); //TODO: Verificar indice
-        }
-        else if (instrucao is InstrucaoFuncao)
-        {
-            var funcao = (InstrucaoFuncao)instrucao;
-            
-            string identificador = funcao.Identificador;
 
-            if(string.IsNullOrWhiteSpace(identificador))
-            {
-                new Erro("Identificador inválido!").LancarErro();
-            }
+        else if(instrucao is InstrucaoSair)
+            InterpretarInstrucaoSair((InstrucaoSair)instrucao);
 
-            if(_programa.FuncaoExiste(identificador))
-            {
-                new ErroFuncaoJaDefinida(identificador).LancarErro();
-            }
-            
-            var novaFuncao = new Funcao(identificador, funcao.Escopo, funcao.Parametros);
-
-            _programa.Funcoes[identificador] = novaFuncao;
-
-        }
         else if (instrucao is InstrucaoChamadaFuncao)
-        {
-            var chamada = (InstrucaoChamadaFuncao)instrucao;
-            var args = chamada.Argumentos.Count;
-            
-            if(chamada.Identificador.StartsWith("__") && chamada.Identificador.EndsWith("__"))
-            {
-                string nomeFuncao = chamada.Identificador.Replace("__", "");
+            InterpretarChamadaFuncao((InstrucaoChamadaFuncao)instrucao);
 
-                MethodInfo funcaoBase = typeof(LibraBase).GetMethod(nomeFuncao, BindingFlags.Static | BindingFlags.Public);
+        else if (instrucao is InstrucaoSe || instrucao is InstrucaoEnquanto)
+            InterpretarCondicional(instrucao);
 
-                if (funcaoBase != null)
-                {
-                    var argsBase = new List<string>();
-                    for(int i = 0; i < args; i++)
-                    {
-                        var expr = InterpretarExpressao(chamada.Argumentos[i]);
+        else if (instrucao is InstrucaoRomper)
+            return instrucao;
 
-                        if (expr == null)
-                            new ErroAcessoNulo().LancarErro();
+        else if (instrucao is InstrucaoRetornar)
+            return instrucao;
 
-                        argsBase.Add(expr.ToString());
-                    }
+        else if (instrucao is InstrucaoFuncao)
+            InterpretarFuncao((InstrucaoFuncao)instrucao);
 
-                    funcaoBase.Invoke(null, argsBase.ToArray());
-                }
-                else
-                {
-                    new ErroFuncaoNaoDefinida(nomeFuncao).LancarErro();
-                }
-            }
-            else
-            {
-                var variaveis = new List<Variavel>();
-                var funcao = _programa.Funcoes[chamada.Identificador];
-                var parametros = funcao.Parametros.Count;
+        new Erro($"Instrução Inválida: {instrucao.ToString()}");
+        return null;
+    }
 
-                if(args != parametros)
-                {
-                    new Erro($"Função {chamada.Identificador}() esperava {parametros} argumento(s) e recebeu {args}").LancarErro();
-                }
+    private void InterpretarInstrucaoSair(InstrucaoSair instrucao)
+    {
+        int? codigoSaida = (int?)InterpretarExpressao(instrucao.Expressao);
 
-                for(int i = 0; i < chamada.Argumentos.Count; i++)
-                {
-                    string nomeVariavel = funcao.Parametros[i];
-                    variaveis.Add(new Variavel(nomeVariavel, new Token(TokenTipo.NumeroLiteral, 0, InterpretarExpressao(chamada.Argumentos[i]).ToString())));
-                }
+        if (codigoSaida == null)
+            new ErroAcessoNulo().LancarErro();
 
-                int retorno = InterpretarEscopo(funcao.Escopo, variaveis);
+        LibraBase.Sair((int)codigoSaida);
+    }
 
-                _ultimoRetorno = retorno; // TODO: melhorar isso 
-            }
-        }
-
-        else if (instrucao is InstrucaoSe)
+    private void InterpretarCondicional(Instrucao instrucao)
+    {
+        if(instrucao is InstrucaoSe)
         {
             var se = (InstrucaoSe)instrucao;
 
             if((int)InterpretarExpressao(se.Expressao) != 0)
-            {
                 InterpretarEscopo(se.Escopo);
-            }
-            else
+            
+            if(se.SenaoEscopo != null)
+                InterpretarEscopo(se.SenaoEscopo);
+            
+            return;
+        }
+
+        var enquanto = (InstrucaoEnquanto)instrucao;
+
+        while((int)InterpretarExpressao(enquanto.Expressao) != 0)
+            InterpretarEscopo(enquanto.Escopo);
+        
+    }
+
+    private void InterpretarFuncao(InstrucaoFuncao funcao)
+    {
+        string identificador = funcao.Identificador;
+
+        if(string.IsNullOrWhiteSpace(identificador))
+        {
+            new Erro("Identificador inválido!").LancarErro();
+        }
+
+        if(_programa.FuncaoExiste(identificador))
+        {
+            new ErroFuncaoJaDefinida(identificador).LancarErro();
+        }
+        
+        var novaFuncao = new Funcao(identificador, funcao.Escopo, funcao.Parametros);
+
+        _programa.Funcoes[identificador] = novaFuncao;
+    }
+
+    private object InterpretarChamadaFuncao(InstrucaoChamadaFuncao chamada)
+    {
+        int qtdArgumentos = chamada.Argumentos.Count;
+
+        if(chamada.Identificador.StartsWith("__") && chamada.Identificador.EndsWith("__"))
+        {
+            string nomeFuncao = chamada.Identificador.Replace("__", "");
+            return ChamarFuncaoInterna(nomeFuncao, chamada);
+        }
+        else
+        {
+            var variaveis = new List<Variavel>();
+            var funcao = _programa.Funcoes[chamada.Identificador];
+            var parametros = funcao.Parametros.Count;
+
+            if(qtdArgumentos != parametros)
+                new Erro($"Função {chamada.Identificador}() esperava {parametros} argumento(s) e recebeu {qtdArgumentos}").LancarErro();
+
+            for(int i = 0; i < chamada.Argumentos.Count; i++)
             {
-                if(se.SenaoEscopo != null)
-                {
-                    InterpretarEscopo(se.SenaoEscopo);
-                }
+                string nomeVariavel = funcao.Parametros[i];
+                variaveis.Add(new Variavel(nomeVariavel, new Token(TokenTipo.NumeroLiteral, 0, InterpretarExpressao(chamada.Argumentos[i]).ToString())));
             }
-        }
-        else if (instrucao is InstrucaoEnquanto)
-        {
-            var enquanto = (InstrucaoEnquanto)instrucao;
 
-            while((int)InterpretarExpressao(enquanto.Expressao) != 0)
-            {
-                InterpretarEscopo(enquanto.Escopo);
-            }
-        }
+            int retorno = InterpretarEscopo(funcao.Escopo, variaveis);
 
-        else if (instrucao is InstrucaoRomper)
-        {
-            return instrucao;
-        }
-
-        else if (instrucao is InstrucaoRetornar)
-        {
-            return instrucao;
+            return _ultimoRetorno = retorno; // TODO: melhorar isso
         }
 
         return null;
+    }
+
+    private object ChamarFuncaoInterna(string nomeFuncao, InstrucaoChamadaFuncao chamada)
+    {
+        MethodInfo funcaoBase = typeof(LibraBase).GetMethod(nomeFuncao, BindingFlags.Static | BindingFlags.Public);
+        int qtdArgumentos = chamada.Argumentos.Count;
+
+        if(funcaoBase == null)
+        {
+            new ErroFuncaoNaoDefinida(nomeFuncao).LancarErro();
+            return null;
+        }
+            
+        var argsBase = new List<string>();
+        for(int i = 0; i < qtdArgumentos; i++)
+        {
+            var expr = InterpretarExpressao(chamada.Argumentos[i]);
+            if (expr == null)
+                new ErroAcessoNulo().LancarErro();
+
+            argsBase.Add(expr.ToString());
+        }
+
+        return funcaoBase.Invoke(null, argsBase.ToArray());
     }
 
     private int InterpretarEscopo(Escopo escopo, List<Variavel> variaveis = null)
@@ -169,13 +165,9 @@ public class Interpretador
         _enderecosIniciaisEscopos.Add(_programa.Variaveis.Count -1);
 
         if(variaveis != null)
-        {
             for(int i = 0; i < variaveis.Count; i++)
-            {
                 _programa.Variaveis.Add(variaveis[i].Identificador, variaveis[i]);
-            }
-        }
-        
+            
         for(int i = 0; i < escopo.Instrucoes.Count; i++)
         {
             var instrucao = InterpretarInstrucao(escopo.Instrucoes[i]);
@@ -185,13 +177,9 @@ public class Interpretador
                 var resultado = InterpretarExpressao(retorno.Expressao);
 
                 if(variaveis != null)
-                {
                     for(int j = 0; j < variaveis.Count; j++)
-                    {
                         _programa.Variaveis.Remove(variaveis[j].Identificador);
-                    }
-                }
-
+                    
                 return (int)resultado;
             }
         }
@@ -213,12 +201,8 @@ public class Interpretador
     private object InterpretarExpressao(Expressao expressao)
     {
         if(expressao is ExpressaoTermo)
-        {
-            var termo = (ExpressaoTermo)expressao;
-
-            return ExtrairValorTermo(termo);
-        }
-
+            return ExtrairValorTermo((ExpressaoTermo)expressao);
+        
         else if(expressao is ExpressaoBinaria)
         {
             var binaria = (ExpressaoBinaria)expressao;
@@ -226,6 +210,7 @@ public class Interpretador
             var esq = (ExpressaoTermo)binaria.Esquerda;
             var dir = binaria.Direita;
 
+            // TODO: Só aceita Inteiros! Modificar
             int a, b = 0;
 
             a = (int)ExtrairValorTermo(esq);
@@ -255,61 +240,56 @@ public class Interpretador
         return 0;
     }
 
-    // TODO: Só um protótipo pra testes. PRECISA DE REFATORAÇÃO!
-    private void DefinirVariavel(string identificador, bool declaracao, bool constante, object valor, TokenTipo tipo, Expressao expressaoIndiceVetor)
+    private object DefinirVariavel(string identificador, bool declaracao, bool constante, object valor, TokenTipo tipo, Expressao expressaoIndiceVetor)
     {
         if(string.IsNullOrWhiteSpace(identificador))
-        {
             new Erro("Identificador inválido!").LancarErro();
-        }
 
         if(declaracao && _programa.VariavelExiste(identificador))
-        {
             new ErroVariavelJaDeclarada(identificador).LancarErro();
-        }
 
         if(_programa.Variaveis.ContainsKey(identificador))
-        {
             if(_programa.Variaveis[identificador].Constante)
                 new ErroModificacaoConstante(identificador).LancarErro();
-        }
-
-        Token token = null;
 
         if (valor is Token)
-            token = (Token)valor;
+            new Variavel(identificador, (Token)valor, constante);
 
-        else if(valor is Expressao)
+        var expressaoValorFinal = (Expressao)valor;
+        object resultado = InterpretarExpressao(expressaoValorFinal);
+
+        if(expressaoIndiceVetor != null)
+            return AtribuirElementoVetor(identificador, expressaoIndiceVetor, expressaoValorFinal);
+        
+        // declarando um Vetor (var x = [10])
+        if (tipo == TokenTipo.Vetor)
         {
-            var expressaoValorFinal = (Expressao)valor;
-            object resultado = InterpretarExpressao(expressaoValorFinal);
+            Token[] tokens = new Token[(int)resultado];
+            for(int i =0; i < tokens.Length; i++)
+                tokens[i] = new Token(TokenTipo.Nulo, 0);
 
-            if(expressaoIndiceVetor != null)
-            {
-                int indiceVetor = (int)InterpretarExpressao(expressaoIndiceVetor);
-                var vetor = (Token[])_programa.Variaveis[identificador].Valor;
-
-                vetor[indiceVetor].Valor = resultado;
-                return;
-            }
-
-            // declarando um Vetor (var x = [10])
-            if (tipo == TokenTipo.Vetor)
-            {
-                Token[] tokens = new Token[(int)resultado];
-                for(int i =0; i < tokens.Length; i++)
-                    tokens[i] = new Token(TokenTipo.Nulo, 0);
-
-                token = new Token(tipo, 0, tokens);
-                
-            }
-            else
-                token = new Token(TokenTipo.NumeroLiteral, 0, resultado);
+            _programa.Variaveis[identificador] = new Variavel(identificador, new Token(tipo, 0, tokens), constante);
+            return null;
         }
 
+        // Declarando uma variável normal
+        var token = new Token(TokenTipo.NumeroLiteral, 0, resultado);
         var variavel = new Variavel(identificador, token, constante);
-
         _programa.Variaveis[identificador] = variavel;
+
+        return variavel.Valor;
+    }
+
+    private object AtribuirElementoVetor(string identificador, Expressao expressaoIndice, Expressao expressao)
+    {
+        int indiceVetor = (int)InterpretarExpressao(expressaoIndice);
+        object valor = InterpretarExpressao(expressao);
+
+        var vetor = (Token[])_programa.Variaveis[identificador].Valor;
+
+        vetor[indiceVetor].Valor = valor;
+
+        return valor;
     }
 
     private object ExtrairValorTermo(ExpressaoTermo termo)
