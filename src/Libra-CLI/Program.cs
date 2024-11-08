1bb0f73d5fab19c5ff8b0e50ab24512d0612d58e
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2024-11-07 22:13:48 -0300

    Adicionado biblioteca padrão

--- src/Libra-CLI/Program.cs
+++ src/Libra-CLI/Program.cs
@@ -1,4 +1,6 @@
-﻿using Libra;
+﻿using System.Reflection;
+using System.Text;
+using Libra;
 using Libra.Arvore;
 
 internal static class Program
@@ -9,6 +11,13 @@ internal static class Program
 
     private const string _ver = "1.0-PREVIEW";
 
+    private static readonly string[] _bibliotecaPadrao = 
+    {
+        "Libra_CLI.so.libra",
+        "Libra_CLI.matematica.libra",
+        "Libra_CLI.utilidades.libra"
+    };
+
     internal static void Main(string[] args)
     {
         _tokenizador = new Tokenizador();
@@ -17,7 +26,7 @@ internal static class Program
 
         if (args.Length == 1)
         {
-            Interpretar(args[0]);
+            Interpretar(args[0], true);
 
             return;
         }
@@ -62,7 +71,34 @@ internal static class Program
         Console.WriteLine("Comandos disponíveis: sair, limpar, licenca, ajuda, interpretar");
     }
 
-    private static Programa Interpretar(string arquivoInicial)
+    private static string LerBibliotecaPadrao()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var sb = new StringBuilder(); 
+
+        foreach (var str in _bibliotecaPadrao)
+        {
+            using (Stream stream = assembly.GetManifestResourceStream(str))
+            {
+                if (stream != null)
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        sb.Append(reader.ReadToEnd());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Recurso não encontrado: {str}");
+                }
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    
+    private static Programa Interpretar(string arquivoInicial, bool incluirPadrao = false)
     {
         if (!File.Exists(arquivoInicial))
         {
@@ -70,8 +106,8 @@ internal static class Program
             return null;
         }
 
-        //var codigoFonte = File.ReadAllText("teste/base.libra").ReplaceLineEndings(Environment.NewLine); 
-        var codigoFonte = File.ReadAllText(arquivoInicial).ReplaceLineEndings(Environment.NewLine); // Sem isso, o Tokenizador buga
+        string codigoFonte = incluirPadrao ? LerBibliotecaPadrao() : string.Empty; // carregando a biblioteca padrão no arquivo
+        codigoFonte += File.ReadAllText(arquivoInicial).ReplaceLineEndings(Environment.NewLine); // Sem isso, o Tokenizador buga
 
         var tokens = _tokenizador.Tokenizar(codigoFonte);
         var programa = _parser.Parse(tokens);
