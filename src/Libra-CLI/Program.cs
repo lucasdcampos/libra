6e7ec6f901fc023aa98428089452b52ce0dd4344
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2024-12-14 21:09:56 -0300

    Refatorado Tokenizador

--- src/Libra-CLI/Program.cs
+++ src/Libra-CLI/Program.cs
@@ -7,13 +7,6 @@ internal static class Program
 {
     private const string _ver = "1.0-PREVIEW";
 
-    private static readonly string[] _bibliotecaPadrao = 
-    {
-        "Libra_CLI.so.libra",
-        "Libra_CLI.matematica.libra",
-        "Libra_CLI.utilidades.libra"
-    };
-
     internal static void Main(string[] args)
     {
         if (args.Length == 1)
@@ -63,33 +56,6 @@ internal static class Program
         Console.WriteLine("Comandos disponíveis: sair, limpar, licenca, ajuda, interpretar");
     }
 
-    private static string LerBibliotecaPadrao()
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        var sb = new StringBuilder(); 
-
-        foreach (var str in _bibliotecaPadrao)
-        {
-            using (Stream stream = assembly.GetManifestResourceStream(str))
-            {
-                if (stream != null)
-                {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        sb.Append(reader.ReadToEnd());
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Recurso não encontrado: {str}");
-                }
-            }
-        }
-
-        return sb.ToString();
-    }
-
-    
     private static void Interpretar(string arquivoInicial, bool incluirPadrao = false)
     {
         if (!File.Exists(arquivoInicial))
@@ -98,8 +64,7 @@ internal static class Program
             return;
         }
 
-        string codigoFonte = incluirPadrao ? LerBibliotecaPadrao() : string.Empty; // carregando a biblioteca padrão no arquivo
-        codigoFonte += File.ReadAllText(arquivoInicial).ReplaceLineEndings(Environment.NewLine); // Sem isso, o Tokenizador buga
+        string codigoFonte = File.ReadAllText(arquivoInicial).ReplaceLineEndings(Environment.NewLine); // Sem isso, o Tokenizador buga
 
         new Interpretador().Interpretar(codigoFonte);
 
--- src/libra/Parser/Tokenizador.cs
+++ src/libra/Parser/Tokenizador.cs
@@ -11,6 +11,23 @@ public class Tokenizador
     private List<Token>? _tokens;
     private int _linha;
     private bool _falha;
+    private Dictionary<string, TokenTipo> _palavrasReservadas = new Dictionary<string, TokenTipo>
+    {
+        { "var", TokenTipo.Var },
+        { "const", TokenTipo.Const },
+        { "funcao", TokenTipo.Funcao },
+        { "se", TokenTipo.Se },
+        { "senao", TokenTipo.Senao },
+        { "enquanto", TokenTipo.Enquanto },
+        { "faca", TokenTipo.Faca },
+        { "romper", TokenTipo.Romper },
+        { "retornar", TokenTipo.Retornar },
+        { "entao", TokenTipo.Entao },
+        { "fim", TokenTipo.Fim },
+        { "ou", TokenTipo.OperadorOu },
+        { "e", TokenTipo.OperadorE }
+    };
+
 
     public List<Token> Tokenizar(string source) 
     {
@@ -25,264 +42,17 @@ public class Tokenizador
             {
                 if(char.IsDigit(Atual()))
                 {
-                    var ponto = false;
-                    while(char.IsDigit(Atual()))
-                    {
-                        texto += ConsumirChar();
-                        if(Atual() == '.') {
-                            if(ponto)
-                                throw new Erro("Número inválido!", _linha);
-
-                            texto += ConsumirChar();
-                            ponto = true;
-                        }
-                    }
-
-                    if(ponto)
-                        AdicionarTokenALista(TokenTipo.NumeroLiteral, double.Parse(texto));
-                    else
-                        AdicionarTokenALista(TokenTipo.NumeroLiteral, int.Parse(texto));
-                    texto = "";
-                    ponto = false;
-                    continue;
+                    TokenizarNumero();
                 }
 
                 else if(char.IsLetter(Atual()) || Atual() == '_')
                 {
-                    texto += ConsumirChar();
-
-                    while(char.IsLetterOrDigit(Atual()) || Atual() == '_')
-                    {
-                        texto += ConsumirChar();
-                    }
-
-                    switch(texto)
-                    {
-                        case "var":
-                            AdicionarTokenALista(TokenTipo.Var);
-                            break;
-                        case "const":
-                            AdicionarTokenALista(TokenTipo.Const);
-                            break;
-                        case "funcao":
-                            AdicionarTokenALista(TokenTipo.Funcao);
-                            break;
-                        case "se":
-                            AdicionarTokenALista(TokenTipo.Se);
-                            break;
-                        case "senao":
-                            AdicionarTokenALista(TokenTipo.Senao);
-                            break;
-                        case "enquanto":
-                            AdicionarTokenALista(TokenTipo.Enquanto);
-                            break;
-                        case "faca":
-                            AdicionarTokenALista(TokenTipo.Faca);
-                            break;
-                        case "romper":
-                            AdicionarTokenALista(TokenTipo.Romper);
-                            break;
-                        case "retornar":
-                            AdicionarTokenALista(TokenTipo.Retornar);
-                            break;
-                        case "entao":
-                            AdicionarTokenALista(TokenTipo.Entao);
-                            break;
-                        case "fim":
-                            AdicionarTokenALista(TokenTipo.Fim);
-                            break;
-                        case "ou":
-                            AdicionarTokenALista(TokenTipo.OperadorOu);
-                            break;
-                        case "e":
-                            AdicionarTokenALista(TokenTipo.OperadorE);
-                            break;
-                        case "importar":
-                            texto = "";
-                            while(Atual() == ' ')
-                                Passar();
-
-                            if(Atual() != '"')
-                                throw new Erro("Esperado `\"`");
-
-                            ConsumirChar();
-
-                            while(Atual() != '"')
-                            {
-                                texto += ConsumirChar();
-                            }
-
-                            ConsumirChar(); // Consumindo `"`
-  
-                            string arquivoCarregado = File.ReadAllText(texto);
-                            _fonte = arquivoCarregado + _fonte;
-                            string linha = $"importar \"{texto}\"";
-                            _fonte = _fonte.Replace(linha, "");
-                            _posicao -= linha.Length;
-                            break;
-                        default:
-                            AdicionarTokenALista(TokenTipo.Identificador, texto);
-                            break;
-                    }
-
-                    texto = "";
-                        
+                    TokenizarPalavra();
                 }
                 else 
                 {
-                    switch(Atual())
-                    {
-                        case ' ':
-                            Passar();
-                            break;
-                        case '\n':
-                            _linha++;
-                            //AdicionarTokenALista(TokenTipo.NovaLinha);
-                            Passar();
-                            break;
-                        case '\r':
-                            Passar();
-                            break;
-                        case '\t':
-                            Passar();
-                            break;
-                        case ';':
-                            AdicionarTokenALista(TokenTipo.PontoEVirgula);
-                            Passar();
-                            break;
-                        case '(':
-                            AdicionarTokenALista(TokenTipo.AbrirParen);
-                            Passar();
-                            break;
-                        case '[':
-                            ConsumirChar();
-                            AdicionarTokenALista(TokenTipo.AbrirCol);
-                            break;
-                        case ']':
-                            AdicionarTokenALista(TokenTipo.FecharCol);
-                            Passar();
-                            break;
-                        case '>':
-                            if(Proximo(1) == '=')
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorMaiorIgualQue);
-                                Passar();
-                                Passar();
-                                break;
-                            }
-
-                            AdicionarTokenALista(TokenTipo.OperadorMaiorQue);
-                            Passar();
-                            break;
-                        case '<':
-                            if(Proximo(1) == '=')
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorMenorIgualQue);
-                                Passar();
-                                Passar();
-                                break;
-                            }
-
-                            AdicionarTokenALista(TokenTipo.OperadorMenorQue);
-                            Passar();
-                            break;
-                        case '!':
-                            if(Proximo(1) == '=')
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorDiferente);
-                                Passar();
-                                Passar();
-                                break;
-                            }
-                            AdicionarTokenALista(TokenTipo.OperadorNeg);
-                            Passar();
-                            break;
-                        case '+':
-                            AdicionarTokenALista(TokenTipo.OperadorSoma);
-                            Passar();
-                            break;
-                        case '-':
-                            AdicionarTokenALista(TokenTipo.OperadorSub);
-                            Passar();
-                            break;
-                        case '*':
-                            AdicionarTokenALista(TokenTipo.OperadorMult);
-                            Passar();
-                            break;
-                        case '/':
-                            Passar();
-                            if (Atual() == '/')
-                            {
-                                ConsumirComentarioLinha();
-                            }
-                            else if (Atual() == '*')
-                            {
-                                ConsumirComentarioBloco();
-                            }
-                            else
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorDiv);
-                            }
-                            break;
-                        case '^':
-                            AdicionarTokenALista(TokenTipo.OperadorPot);
-                            Passar();
-                            break;
-                        case ')':
-                            AdicionarTokenALista(TokenTipo.FecharParen);
-                            Passar();
-                            break;
-                        case '=':
-                            ConsumirChar();
-
-                            if(Atual() == '=')
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorComparacao);
-                                Passar();
-                            }
-                            else
-                            {
-                                AdicionarTokenALista(TokenTipo.OperadorDefinir);
-                            }
-                            break;
-                        case '"':
-                            Passar();
-
-                            while(Atual() != '"')
-                            {
-                                if(Atual() == '\\' && Proximo(1) == '"')
-                                {
-                                    Passar();
-                                }
-                                texto += ConsumirChar();
-                            }
-
-                            AdicionarTokenALista(TokenTipo.TextoLiteral, texto);
-                            texto = "";
-                            Passar();
-                            break;
-                        case '\'':
-                            Passar();
-                            texto += ConsumirChar();
-                            AdicionarTokenALista(TokenTipo.CaractereLiteral, texto);
-                            texto = "";
-                            if(Atual() != '\'')
-                            {
-                                throw new Erro("Esperado `'`", _linha);
-                            }
-                            Passar();
-                            break;
-                        case ',':
-                            AdicionarTokenALista(TokenTipo.Virgula);
-                            Passar();
-                            break;
-                        default:
-                            throw new ErroTokenInvalido($"{Atual()} (ASCII = {(int)Atual()})", _linha);
-                            break;
-                    }
-                    
+                    TokenizarSimbolo();
                 }
-
             }
         
             AdicionarTokenALista(TokenTipo.FimDoArquivo);
@@ -298,6 +68,253 @@ public class Tokenizador
         }
 
         return null;
+    }
+
+    private void TokenizarNumero()
+    {
+        string buffer = "";
+        var ponto = false;
+        while (char.IsDigit(Atual()))
+        {
+            buffer += ConsumirChar();
+            if (Atual() == '.')
+            {
+                if (ponto)
+                    throw new Erro("Número inválido!", _linha);
+
+                buffer += ConsumirChar();
+                ponto = true;
+            }
+        }
+
+        if (ponto)
+            AdicionarTokenALista(TokenTipo.NumeroLiteral, double.Parse(buffer));
+        else
+            AdicionarTokenALista(TokenTipo.NumeroLiteral, int.Parse(buffer));
+    }
+
+    private void TokenizarPalavra()
+    {
+        string buffer = "" + ConsumirChar();
+
+        while (char.IsLetterOrDigit(Atual()) || Atual() == '_')
+        {
+            buffer += ConsumirChar();
+        }
+
+        if (_palavrasReservadas.ContainsKey(buffer))
+        {
+            AdicionarTokenALista(_palavrasReservadas[buffer]);
+        }
+        else if (buffer == "importar")
+        {
+            buffer = "";
+            while (Atual() == ' ')
+                Passar();
+
+            if (Atual() != '"')
+                throw new Erro("Esperado `\"`");
+
+            ConsumirChar();
+
+            buffer += ConsumirAte('"');
+
+            string caminhoExecutavel = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, buffer);
+            string caminhoUsuario = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), buffer);
+
+            if (!File.Exists(caminhoExecutavel) && !File.Exists(caminhoUsuario))
+                throw new ErroAcessoNulo(caminhoExecutavel);
+
+            string caminhoArquivo = File.Exists(caminhoExecutavel) ? caminhoExecutavel : caminhoUsuario;
+
+            string arquivoCarregado = File.ReadAllText(caminhoArquivo);
+            _fonte = arquivoCarregado + _fonte;
+            string linha = $"importar \"{buffer}\"";
+            _fonte = _fonte.Replace(linha, "");
+            _posicao -= linha.Length;
+        }
+        else
+        {
+            AdicionarTokenALista(TokenTipo.Identificador, buffer);
+        }
+
+    }
+
+    private void TokenizarSimbolo()
+    {
+        string buffer = "";
+        switch (Atual())
+        {
+            case ' ':
+                Passar();
+                break;
+            case '\n':
+                _linha++;
+                Passar();
+                break;
+            case '\r':
+                Passar();
+                break;
+            case '\t':
+                Passar();
+                break;
+            case ';':
+                AdicionarTokenALista(TokenTipo.PontoEVirgula);
+                Passar();
+                break;
+            case '(':
+                AdicionarTokenALista(TokenTipo.AbrirParen);
+                Passar();
+                break;
+            case '[':
+                ConsumirChar();
+                AdicionarTokenALista(TokenTipo.AbrirCol);
+                break;
+            case ']':
+                AdicionarTokenALista(TokenTipo.FecharCol);
+                Passar();
+                break;
+            case '>':
+                if (Proximo(1) == '=')
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorMaiorIgualQue);
+                    Passar();
+                    Passar();
+                    break;
+                }
+
+                AdicionarTokenALista(TokenTipo.OperadorMaiorQue);
+                Passar();
+                break;
+            case '<':
+                if (Proximo(1) == '=')
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorMenorIgualQue);
+                    Passar();
+                    Passar();
+                    break;
+                }
+
+                AdicionarTokenALista(TokenTipo.OperadorMenorQue);
+                Passar();
+                break;
+            case '!':
+                if (Proximo(1) == '=')
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorDiferente);
+                    Passar();
+                    Passar();
+                    break;
+                }
+                AdicionarTokenALista(TokenTipo.OperadorNeg);
+                Passar();
+                break;
+            case '+':
+                AdicionarTokenALista(TokenTipo.OperadorSoma);
+                Passar();
+                break;
+            case '-':
+                AdicionarTokenALista(TokenTipo.OperadorSub);
+                Passar();
+                break;
+            case '*':
+                AdicionarTokenALista(TokenTipo.OperadorMult);
+                Passar();
+                break;
+            case '/':
+                Passar();
+                if (Atual() == '/')
+                {
+                    ConsumirComentarioLinha();
+                }
+                else if (Atual() == '*')
+                {
+                    ConsumirComentarioBloco();
+                }
+                else
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorDiv);
+                }
+                break;
+            case '^':
+                AdicionarTokenALista(TokenTipo.OperadorPot);
+                Passar();
+                break;
+            case ')':
+                AdicionarTokenALista(TokenTipo.FecharParen);
+                Passar();
+                break;
+            case '=':
+                ConsumirChar();
+
+                if (Atual() == '=')
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorComparacao);
+                    Passar();
+                }
+                else
+                {
+                    AdicionarTokenALista(TokenTipo.OperadorDefinir);
+                }
+                break;
+            case '"':
+                Passar();
+
+                while (Atual() != '"')
+                {
+                    if (Atual() == '\\' && Proximo(1) == '"')
+                    {
+                        Passar();
+                    }
+                    buffer += ConsumirChar();
+                }
+
+                AdicionarTokenALista(TokenTipo.TextoLiteral, buffer);
+                buffer = "";
+                Passar();
+                break;
+            case '\'':
+                Passar();
+                buffer += ConsumirChar();
+                AdicionarTokenALista(TokenTipo.CaractereLiteral, buffer);
+                buffer = "";
+                if (Atual() != '\'')
+                {
+                    throw new Erro("Esperado `'`", _linha);
+                }
+                Passar();
+                break;
+            case ',':
+                AdicionarTokenALista(TokenTipo.Virgula);
+                Passar();
+                break;
+            default:
+                throw new ErroTokenInvalido($"{Atual()} (ASCII = {(int)Atual()})", _linha);
+                break;
+        }
+    }
+
+    private string ConsumirAte(char caractere)
+    {
+        string buffer = "";
+        while(Atual() != caractere)
+        {
+            buffer += ConsumirChar();
+
+            if (Atual() == '\0')
+                throw new Erro($"Esperado `{caractere}`");
+        }
+
+        Passar();
+        return buffer;
+    }
+
+    private void ConsumirEspacosVazios()
+    {
+        while(Atual() == ' ' || Atual() == '\n')
+        {
+            Passar();
+        }
     }
 
     private void ConsumirComentarioLinha()
