fe24900136389dd4932a8fbad943696d1f3e79b7
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-05-28 22:05:30 -0300

    Adicionado Libra.Testes

---
+++ src/Libra.Testes/Program.cs
@@ -0,0 +1,8 @@
+﻿using Libra.Api;
+
+string codigo = "1+2*3";
+var motor = new MotorLibra();
+var saida = motor.Executar(codigo);
+
+Console.WriteLine("Código: " + codigo);
+Console.WriteLine($"Saída: {saida}");
